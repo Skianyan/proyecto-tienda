e29b8f9f5fb5b58632259b48e989c86d9549aa66
Ricardo Haro <99053383+Skianyan@users.noreply.github.com>
2023-11-27 10:58:19 -0800

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,9 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product',[ProductController::class, 'index'])->name('product.index');
-Route::get('/product/create',[ProductController::class, 'create'])->name('product.create');
-Route::post('/product',[ProductController::class, 'store'])->name('product.store');
-Route::get('/product/{product}/edit',[ProductController::class, 'edit'])->name('product.edit');
-Route::put('/product/{product}/update',[ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/{product}/delete',[ProductController::class, 'delete'])->name('product.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
