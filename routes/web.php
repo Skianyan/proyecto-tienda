619082d3b88268a289ffb9b9a7b1182a337cbcf7
Ricardo Haro <99053383+Skianyan@users.noreply.github.com>
2023-12-04 14:46:34 -0800

    protected routes

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware('auth')->group(function () {
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
 Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
 Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
 Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
-
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
