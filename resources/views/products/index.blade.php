191f7852b497b28f6c5cd2e16e3bb64d68f82bf1
Skianyan <99053383+Skianyan@users.noreply.github.com>
2023-12-11 10:04:21 -0800

    update

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,76 +4,117 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600&display=swap">
+
+    
     <title>Document</title>
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: flex-start;
+            min-height: 100vh;
+            background-size: 100% 100%;
+            animation: gradientAnimation 14s ease infinite;
+            color: #fff;
+            background-image: url('https://i.gifer.com/DbI.gif');
+            
+        }
+
+        @keyframes gradientAnimation {
+            100% { background-position: 100%; }
+            50% { background-position: 50% ; }
+            50% { background-position:  50%; }
+        }
+        .card-body {
+            background: #e9ecef;
+            margin: 20px;
+            padding: 20px;
+            border-radius: 80px;
+            text-align: center;
+        }
+        .card-text {
+            font-size: 1.2rem;
+            color: #343a40;
+            line-height: 1.6;
+        }
+        .card {
+        transition: background-color 0.4s ease, color 0.4s ease;
+    }
+
+    .card-body:hover {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        transform: scale(1.05);
+        background-color: black;
+        color: white;
+    }
+    .card-body:hover p {
+        color: #eeeeee;
+    }
+
+    </style>
 </head>
-<body class="antialiased">
-    <div class="bg-slate-600 h-[100vh]">
-        @if (Route::has('login'))
-                <div class="sm:top-0 sm:right-0 p-6 text-right z-10 bg-dark text-white">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-white hover:text-black dark:text-gray-400 dark:hover:text-blue-100 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-white hover:text-black dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+<body class="antialiased ">
+    
+    
+    @if (Route::has('login'))
+    <div class="sm:top-0 sm:right-0 p-6 text-right z-10 bg-dark text-white">
+        @auth
+            <a href="{{ url('/dashboard') }}" class=" font-semibold text-black hover:text-black dark:text-gray-400 dark:hover:text-blue-100 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="text-xl font-semibold text-black hover:text-white dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-white hover:text-black dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="text-xl ml-4 font-semibold text-black hover:text-white dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
             @endif
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div class="container">
-                @if(session()->has('success'))
-                    <div class="text-white">
-                        {{session('success')}}
-                    </div>
-                @endif
-                @auth
-                <div class="w-100 d-flex justify-content-end">
-                    <a class="bg-slate-800 text-white p-4 rounded-lg" href="{{route('product.create')}}">Create New Product</a>
-                </div>      
-                @endauth
-                <table class="table table-dark table-striped mt-3">
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Qty</th>
-                        <th>Price</th>
-                        <th>Description</th>
-                        @auth
-                        <th>Options</th>
-                        <th>Delete</th>
-                        @endauth
-                    </tr>
-                    @foreach ($products as $product)
-                        <tr>
-                            <td>{{$product->id}}</td>
-                            <td>{{$product->name}}</td>
-                            <td>{{$product->qty}}</td>
-                            <td>{{$product->price}}</td>
-                            <td>{{$product->description}}</td>
+        @endauth
+    </div>
+    @endif
+    
+    <div class="container mt-3">
+        @if(session()->has('success'))
+            <div class="alert alert-success text-white">
+                {{ session('success') }}
+            </div>
+        @endif
 
-                            @auth           
-                                
-                            <td>
-                                <a href="{{route('product.edit',['product' => $product])}}">Edit</a>
-                            </td>
-                            <td>
-                                <form method="post" action="{{route('product.destroy',['product' => $product])}}">
+        @auth
+            <div class="d-flex justify-content-end">
+                <a class="bg-slate-800 text-white p-4 rounded-lg" href="{{ route('product.create') }}">Create New Product</a>
+            </div>
+        @endauth
+
+        <div class="row">
+            @foreach ($products as $product)
+                <div class="col-md-4 mb-4 ">
+                    <div class="card text-black ">
+                        <div class="card-body p-4 hover:shadow-md">
+                            <h5 class=" text-2xl font-bold">{{ $product->name }}</h5>
+                            <p class="card-text">Qty: {{ $product->qty }}</p>
+                            <p class="card-text">Price: {{ $product->price }}</p>
+                            <p class="card-text">{{ $product->description }}</p>
+
+                            @auth
+                                <a href="{{ route('product.edit', ['product' => $product]) }}" class="btn btn-primary">Edit</a>
+                                <form method="post" action="{{ route('product.destroy', ['product' => $product]) }}">
                                     @csrf
                                     @method('delete')
-                                    <input type="submit" value="Delete">
+                                    <button type="submit" class="btn btn-danger">Delete</button>
                                 </form>
-                            </td>
-                            
                             @endauth
-                        </tr>
-                    @endforeach
-                </table>
-            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
+
     <script src="https://cdn.tailwindcss.com"></script>
 </body>
 </html>
